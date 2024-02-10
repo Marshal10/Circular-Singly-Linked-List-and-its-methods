5445bc5b5cfe01780c4a5a556d0fe322846d4ad1
Marshal10 <marshalmaleane123@gmail.com>
2024-02-10 17:10:09 +0530

    Prepend

--- main.py
+++ main.py
@@ -32,10 +32,22 @@ class CircularSinglyLinkedList:
             self.tail=new_node
         self.length+=1
         
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+            new_node.next=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+            self.tail.next=new_node
+        self.length+=1
+        
 cs_linked_list=CircularSinglyLinkedList()
+cs_linked_list.prepend(50)
+cs_linked_list.prepend(90)
+cs_linked_list.append(100)
 print(cs_linked_list)
-cs_linked_list.append(10)
-cs_linked_list.append(20)
-cs_linked_list.append(30)
 
-print(cs_linked_list)
+# print(cs_linked_list)
