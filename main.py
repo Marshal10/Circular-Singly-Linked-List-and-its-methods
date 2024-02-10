fa3c1cc8c6dfb7f361e26bfcfde22c11bd96b6b0
Marshal10 <marshalmaleane123@gmail.com>
2024-02-10 17:21:22 +0530

    Insert method

--- main.py
+++ main.py
@@ -44,10 +44,35 @@ class CircularSinglyLinkedList:
             self.tail.next=new_node
         self.length+=1
         
+    def insert(self,index,value):
+        new_node=Node(value)
+        if index < -1  or index > self.length:
+            raise Exception("Index out of range")
+        if index==0:  
+            if self.head is None:
+                self.head=new_node
+                self.tail=new_node
+                new_node.next=new_node
+            else:
+                new_node.next=self.head
+                self.head=new_node
+                self.tail.next=new_node
+        elif index==self.length or index==-1:
+            self.tail.next=new_node
+            new_node.next=self.head
+            self.tail=new_node
+        else:
+            temp=self.head
+            for _ in range(index-1):
+                temp=temp.next
+            new_node.next=temp.next
+            temp.next=new_node
+        self.length+=1
+        
 cs_linked_list=CircularSinglyLinkedList()
 cs_linked_list.prepend(50)
 cs_linked_list.prepend(90)
 cs_linked_list.append(100)
 print(cs_linked_list)
-
-# print(cs_linked_list)
+cs_linked_list.insert(1,70)
+print(cs_linked_list)
